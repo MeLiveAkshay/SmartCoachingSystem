59577fb47c2c5acaa122f85fa1e4751dc2a50b43
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-07-10 23:01:42 +0530

    Add Contact Us page and update views for improved user interaction

--- SmartCoachingWebApplication/page/views.py
+++ SmartCoachingWebApplication/page/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("College Manage+ment System")
+
+
+def insertContactUs(request):
+    return render(request, 'contact.html')    
